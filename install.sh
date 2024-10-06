33e066c944cd53ffe128656f481b73df5794fbb3
minxie <laba22@163.com>
2024-10-06 22:42:20 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,13 @@ pip install torch
 pip install rich
 pip install transformers
 pip install nltk
+pip install deepfilternet
+pip install duckduckgo_search
+pip install unidic-lite
+pip install 'accelerate>=0.26.0'
+pip install bitsandbytes
 pip install git+https://github.com/2noise/ChatTTS.git
 pip install git+https://github.com/andimarafioti/MeloTTS.git#egg=MeloTTS
+
+python -m unidic download
+
